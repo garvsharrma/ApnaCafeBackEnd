2590a01404a2f09d96a939bf874965779e2a6358
garvsharrma <133889481+garvsharrma@users.noreply.github.com>
2024-09-23 20:17:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://garvsharrma.github.io', // Your GitHub Pages domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 app.use(bodyParser.json());
 
 // Nodemailer transporter setup for Ethereal
